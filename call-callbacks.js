0fd3304e4c39310f011e399b8f645578307e789f
Derek <derlloyd@outlook.com>
2016-04-19 19:26:29 +0000

    first and last callbacks

--- call-callbacks.js
+++ call-callbacks.js
@@ -3,15 +3,34 @@
 
 function firstChar(str, callback) {
     var first = str[0];
-    callback(first);
+    setTimeout(function() {
+        callback(first);
+    }, 1000);
 }
 
-firstChar("Hello", function(x) {
+// firstChar("Hello", console.log); 
+
+
+//  function called lastChar that takes a string 
+//  and "returns" the last character of the string after one second
+
+function lastChar(str, callback) {
+    var last = str[str.length - 1];
     setTimeout(function() {
-        console.log(x);
+        callback(last);
     }, 1000);
+}
+
+// lastChar("long string", console.log);
+
+
+
+function getFirstAndLast(str, callback) {
+    firstChar(str, callback) + lastChar(str, callback);
+};
+
+getFirstAndLast("hello", function(firstLast) {
+    console.log(firstLast); // should output "ho"
 });
-
-
 
 
